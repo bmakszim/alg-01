8bff7b566f434629d1d6892811f92849276ad7c8
bmakszim <22057990+bmakszim@users.noreply.github.com>
2024-12-09 18:03:28 +0100

    Add reading from and writing to standard in adn output

---
+++ src/main/java/greedy/Candies.java
@@ -0,0 +1,39 @@
+package greedy;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class Candies {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(_ -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+
+        long result = candies(n, arr);
+
+        System.out.println(result);
+    }
+
+    public static long candies(int n, List<Integer> arr) {
+        return 0;
+    }
+}
